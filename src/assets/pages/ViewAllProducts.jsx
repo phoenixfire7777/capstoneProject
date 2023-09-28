d10f58b55b4cdf034e86dda27710c827a74905c3
phoenixfire77777 <phoenixfire77777@gmail.com>
2023-09-28 19:57:20 -0400

    added filter and sort components to products page

--- src/assets/pages/ViewAllProducts.jsx
+++ src/assets/pages/ViewAllProducts.jsx
@@ -9,19 +9,21 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 //this page allows user to view all products
-export default function ViewAllProducts({products, setProducts, cart, setCart, addToCart, token, authenticated, setAuthenticated}) {
+export default function ViewAllProducts({ products, setProducts, cart, setCart, addToCart, token, authenticated, setAuthenticated }) {
     const [categories, setCategories] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState("all")
+    const [sortBy, setSortBy] = useState("all")
     console.log("this is before " + authenticated)
 
 
     useEffect(() => {
-        
+
         async function fetchData() {
-            if(token !== null){
-            setAuthenticated(true)
-            console.log("this is after " + authenticated)
+            if (token !== null) {
+                setAuthenticated(true)
+                console.log("this is after " + authenticated)
             }
-            else{
+            else {
                 setAuthenticated(false)
             }
             const data = await fetchAllProducts()
@@ -34,26 +36,58 @@ export default function ViewAllProducts({products, setProducts, cart, setCart, a
         fetchData()
     }, [])
 
+    function selectCategory(e) {
+        setSelectedCategory(e.target.value)
+    }
+    function selectSort(e) {
+        setSortBy(e.target.value)
+    }
+
+    let filteredProducts = products
+
+    if (selectedCategory !== "all") {
+        filteredProducts = products.filter(product => product.category === selectedCategory);
+    }
+
+    if (sortBy === 'price') {
+        filteredProducts.sort((a, b) => a.price - b.price)
+    }
+    else if (sortBy === 'rating') {
+        filteredProducts.sort((a, b) => a.rating.rate - b.rating.rate)
+    }
     console.log(categories)
     return (
         <div>
-             <Row>
-             <Form.Select aria-label="Default select example">
-      <option>Filter</option>
-      {
-        categories.map(category => (
-            <option value={category} key={category}>{category}</option>
-        ))
-      }
-    </Form.Select>
-             </Row>
+            <Row>
+                <Col>
+                    <Form.Select onChange={selectCategory} aria-label="Default select example">
+                        <option value='all'>Filter</option>
+                        {
+                            categories.map(category => (
+                                <option value={category} key={category}>{category}</option>
+                            ))
+                        }
+
+                    </Form.Select>
+                </Col>
+                <Col>
+
+                    <Form.Select onChange={selectSort} aria-label="Default select example">
+                        <option value={sortBy}>Sort</option>
+                        <option value="price">Price</option>
+                        <option value="rating">Rating</option>
+
+                    </Form.Select>
+                </Col>
+            </Row>
+
             <Row xs={'auto'} sm={'auto'} md={'auto'} lg={'auto'} xl={'auto'} xxl={'auto'} className="g-4">
                 {
                     Array.from({ length: 1 }).map((_, idx) => (
 
 
-                        products.map((product, index) => {
-                            
+                        filteredProducts.map((product, index) => {
+
                             return (
                                 <Col key={index}>
                                     <ProductCard
